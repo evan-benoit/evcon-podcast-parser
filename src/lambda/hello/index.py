2ac47d01ec4d9f8a7f03dd842a144ffbaaebab44
Evan Benoit <evan.m.benoit@gmail.com>
2025-10-14 16:05:06 -0400

    making an API call from my lambda, yay

--- src/lambda/hello/index.py
+++ src/lambda/hello/index.py
@@ -1,5 +1,16 @@
+import boto3
+import json
+
+sts = boto3.client("sts")
+
 def handler(event, context):
+    identity = sts.get_caller_identity()
+
+    i = 5
+    j = 2
+    k = i + j
+
     return {
         "statusCode": 200,
-        "body": "Hello from Lambda!"
+        "body": "HELLO WORLD " + json.dumps(identity)
     }
---
+++ tf/bedrock.tf
@@ -0,0 +1,4 @@
+resource "aws_iam_role_policy_attachment" "lambda_bedrock" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"
+}
