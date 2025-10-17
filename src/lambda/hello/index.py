19777920b6dc135d5cc9335e575de1ad1d15aa54
Evan Benoit <evan.m.benoit@gmail.com>
2025-10-17 14:03:08 -0400

    refactored some functions

--- src/lambda/hello/index.py
+++ src/lambda/hello/index.py
@@ -1,14 +1,34 @@
 import boto3
 import json
 
+bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
 
 def handler(event, context):
+    transcript = event.get("transcript")
+    if not transcript:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Transcript not provided"})
+        }
+    
+    summary = get_summary(transcript)
 
-    bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
+    returnJson = {"summary": summary}
 
-    transcript = event.get("transcript", "No transcript provided.")
-    prompt = f"Summarize the following podcast transcript excerpt:\n\n{transcript}"
+    return {
+        "statusCode": 200,
+        "body": json.dumps(returnJson)
+    }
 
+def get_summary(transcript):
+    prompt = f"Summarize the following podcast transcript excerpt in 200-300 words:\n\n{transcript}"
+    response = invoke_model(prompt)
+    result = json.loads(response['body'].read())
+    output_text = result["content"][0]["text"]
+    return output_text
+
+
+def invoke_model(prompt):
     response = bedrock.invoke_model(
         modelId='anthropic.claude-3-sonnet-20240229-v1:0',  # or other Claude model IDs
         accept="application/json",
@@ -22,11 +42,5 @@ def handler(event, context):
             ]
         })
     )
-
-    result = json.loads(response['body'].read())
-    output_text = result["content"][0]["text"]
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"summary": output_text})
-    }
+    
+    return response
